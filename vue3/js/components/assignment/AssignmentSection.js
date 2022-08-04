9d1744eb370dcf50933f5851967b1bc02ae42903
Hafijul Islam <hafijul2332gmail.com>
2022-08-04 20:16:42 +0600

    filter optimizations

--- vue3/js/components/assignment/AssignmentSection.js
+++ vue3/js/components/assignment/AssignmentSection.js
@@ -2,15 +2,15 @@ import AssignmentList from "./AssignmentList.js";
 
 export default {
     template: `
-      <assignment-list title="In Progress" :assignments="inProgressAssignments"></assignment-list>
-      <assignment-list title="Completed" :assignments="completedAssignments"></assignment-list>
+      <assignment-list title="In Progress" :assignments="filters.inProgress"></assignment-list>
+      <assignment-list title="Completed" :assignments="filters.completed"></assignment-list>
     `,
     data() {
         return {
             assignments: [
                 {id: 1, name: "Finish Project", complete: false},
                 {id: 2, name: "Add Login Added", complete: false},
-                {id: 3, name: "Read Documentations", complete: false},
+                {id: 3, name: "Read Documentations", complete: true},
             ]
         }
     },
@@ -18,11 +18,11 @@ export default {
         'assignment-list': AssignmentList
     },
     computed: {
-        completedAssignments() {
-            return this.assignments.filter((assignment) => assignment.complete);
-        },
-        inProgressAssignments() {
-            return this.assignments.filter((assignment) => !assignment.complete);
+        filters() {
+            return {
+                completed: this.assignments.filter((assignment) => assignment.complete),
+                inProgress: this.assignments.filter((assignment) => !assignment.complete),
+            }
         }
     }
 }
