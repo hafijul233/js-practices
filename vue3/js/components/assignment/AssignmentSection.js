39b77ec9ef37231ee7cff7133aba11eafa393c6c
Hafijul Islam <hafijul2332gmail.com>
2022-08-05 20:39:53 +0600

    tag section added

--- vue3/js/components/assignment/AssignmentSection.js
+++ vue3/js/components/assignment/AssignmentSection.js
@@ -1,9 +1,10 @@
 import AssignmentList from "./AssignmentList.js";
 import AssignmentForm from "./AssignmentForm.js";
 
-export default   {
+export default {
     template: `
       <section class="space-y-6">
+
       <assignment-list
           title="In Progress"
           :assignments="filters.inProgress"/>
@@ -12,26 +13,29 @@ export default   {
           title="Completed"
           :assignments="filters.completed"/>
 
-      <assignment-form @addAssignment="addAssignment"/>
+      <assignment-form
+          @addAssignment="addAssignmentMethod"/>
+
       </section>
     `,
     data() {
         return {
             assignments: [
-                {id: 1, name: "Finish Project", complete: false, tags : ['math']},
-                {id: 2, name: "Add Login Added", complete: false, tags : ['science']},
-                {id: 3, name: "Read Documentations", complete: true, tags : ['travel']},
-                {id: 4, name: "Research & Development", complete: false, tags : ['math', 'science', 'reading']},
+                {id: 1, name: "Finish Project", complete: false, tag: 'math'},
+                {id: 2, name: "Add Login Added", complete: false, tag: 'science'},
+                {id: 3, name: "Read Documentations", complete: true, tag: 'travel'},
+                {id: 4, name: "Research & Development", complete: false, tag: 'math'},
             ]
         }
     },
     methods: {
-        addAssignment(title) {
+        addAssignmentMethod(title) {
 
             this.assignments.push({
                 name: title,
                 complete: false,
-                id: (this.assignments.length + 1)
+                id: (this.assignments.length + 1),
+                tag: 'default'
             })
         }
     },
